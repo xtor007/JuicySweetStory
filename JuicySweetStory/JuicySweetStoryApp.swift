b3a4dda6662603df1b7b3cff66cc11ab8338adee
Anatolii Khramchenko <71408259+xtor007@users.noreply.github.com>
2023-02-24 13:57:20 +0200

    Initial Commit

---
+++ JuicySweetStory/JuicySweetStoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  JuicySweetStoryApp.swift
+//  JuicySweetStory
+//
+//  Created by Anatoliy Khramchenko on 24.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct JuicySweetStoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
