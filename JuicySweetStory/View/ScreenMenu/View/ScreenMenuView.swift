85d005691d06cce8abfa13295a7ab5b4b50e47e2
Anatolii Khramchenko <71408259+xtor007@users.noreply.github.com>
2023-02-24 16:10:21 +0200

    Create ScreenMenuView

--- JuicySweetStory/View/ScreenMenu/View/ScreenMenuView.swift
+++ JuicySweetStory/View/ScreenMenu/View/ScreenMenuView.swift
@@ -10,15 +10,20 @@ import SwiftUI
 struct ScreenMenuView: View {
 
     @State var elements: [ScreenMenuElement]
+    let callback: (ScreenMenuElement) -> Void
 
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 16) {
+            ForEach(0..<elements.count, id: \.self) { elementIndex in
+                ScreenMenuElementView(element: elements[elementIndex], callback: callback)
+            }
+        }
     }
 
 }
 
 struct ScreenMenuView_Previews: PreviewProvider {
     static var previews: some View {
-        ScreenMenuView(elements: ScreenMenuElementPreview.preview)
+        ScreenMenuView(elements: ScreenMenuElementPreview.preview) { _ in }
     }
 }
